5dfb545c810a64a56d54e258da297cab0efe4ada
MennaEhab <mennaehab21.5@gmail.com>
2020-02-29 11:45:07 +0200

    Add TMU module

---
+++ Car_Static_Architecture/Car_Static_Architecture/TMU_main.c
@@ -0,0 +1,58 @@
+/*
+ * Dynamic_design.c
+ *
+ * Created: 2/24/2020 12:11:07 PM
+ * Author : MENA
+ */ 
+
+#include "DIO.h"
+#include "TMU.h"
+#include "avr/interrupt.h"
+
+
+void toggle1(void){
+	
+	PORTA_DATA ^= 0x0F ;
+	
+}
+
+void toggle2(void){
+	
+	PORTA_DATA ^= 0xF0 ;
+	
+}
+
+int main(void)
+{
+    /* Replace with your application code */
+	
+	sei();
+	DDRA = 0xFF ;
+	TMU_Cfg_s TMU_cfg1 ;
+	TMU_cfg1.Timer_channel = TMU_TIMER_CH2 ;
+	TMU_cfg1.TMU_Reselution = reselution_one_mili ;
+	
+	TMU_Init(&TMU_cfg1);
+	
+	TMU_tsak_s TMU_task1 ;
+	TMU_task1.delay_time = 1000;
+	TMU_task1.Ptr_FunctionCall = toggle1;
+	TMU_task1.Task_ID = 1;
+	TMU_task1.TMU_mode = PERIODIC ;
+	
+	TMU_Start_Timer(&TMU_task1) ;
+	
+	TMU_tsak_s TMU_task2 ;
+	TMU_task2.delay_time = 500;
+	TMU_task2.Ptr_FunctionCall = toggle2;
+	TMU_task2.Task_ID = 2;
+	TMU_task2.TMU_mode = PERIODIC ;
+	
+	TMU_Start_Timer(&TMU_task2) ;
+	
+    while (1) 
+    {
+		TMU_Dispatch();
+    }
+}
+
